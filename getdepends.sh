896d948007f1ccfc3efe8340437c3aecf602a3f3
Faizu <102569285+faizu396@users.noreply.github.com>
2023-12-01 20:45:39 -0600

    Update getdepends.sh

--- getdepends.sh
+++ getdepends.sh
@@ -2,10 +2,10 @@
 
 pkg install git wget make python getconf zip apksigner clang binutils
 wget https://github.com/libsdl-org/SDL/releases/download/release-2.28.5/SDL2-2.28.5.zip
-unzip -q SDL2-2.0.12.zip
+unzip -q SDL2-2.28.5.zip
 mv SDL2-2.0.12/include include
 ln -s . include/SDL2
-rm SDL2-2.0.12.zip
-rm -r SDL2-2.0.12
+rm SDL2-2.28.5.zip
+rm -r SDL2-2.28.5
 mkdir ~/../usr/include/KHR
 wget https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h -O ~/../usr/include/KHR/khrplatform.h
