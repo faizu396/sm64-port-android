621b9cd8a9af89c0e3bdfcacb530ded7ac3af1f5
Faizu <102569285+faizu396@users.noreply.github.com>
2023-12-01 20:50:06 -0600

    Update getdepends.sh

--- getdepends.sh
+++ getdepends.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkg install git wget make python getconf zip apksigner clang binutils
+pkg install git wget make python getconf zip apksigner clang binutils openssl
 wget https://github.com/libsdl-org/SDL/releases/download/release-2.28.5/SDL2-2.28.5.zip
 unzip -q SDL2-2.28.5.zip
 mv SDL2-2.0.12/include include
